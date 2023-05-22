3e8635c447b08c94560c3064a05ce7f2dc5df308
NursahDemirpolat <nursahdmrplt@gmail.com>
2023-05-22 18:04:10 +0300

    Kurs resimlerini basma

--- src/Course.js
+++ src/Course.js
@@ -3,12 +3,19 @@ import Bootstrap from './images/bootstrap5.png'
 import Ccsharp from './images/ccsharp.png'
 import Kompleweb from './images/kompleweb.jpg'
 
+const courseMap = {
+    Angular:Angular,
+    Bootstrap,
+    Ccsharp,
+    Kompleweb, //key ve value aynı isimse : yapmak zorunluluğu yok
+}
 
 function Course ({courseName}) {
     console.log(courseName);
+    console.log(courseMap[courseName]);
     return (  
         <div>
-            <img src={Angular} alt="" />
+            <img src={courseMap[courseName]} alt={courseName} />
         </div>
     );
 }
